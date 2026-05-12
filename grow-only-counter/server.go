a446baf328327167cea69100cfac4efdac152644
Piotr Rudnicki <piotr.rudnicki94@protonmail.com>
2026-05-12 13:50:11 +0200

    grow only counter solved

--- grow-only-counter/server.go
+++ grow-only-counter/server.go
@@ -1,34 +1,49 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
+	"math/rand"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+const counterKey = "counter"
+
 type Server struct {
 	node   *maelstrom.Node
+	kv     *maelstrom.KV
 	nodeID string
 }
 
-type EchoMessage struct {
-	Type  string `json:"type"`
-	MsgID int64  `json:"msg_id"`
-	Echo  string `json:"echo"`
+type ReadMessage struct {
+	Type string `json:"type"`
 }
 
-type EchoMessageResponse struct {
+type ReadMessageResponse struct {
 	Type  string `json:"type"`
-	MsgID int64  `json:"msg_id"`
-	Echo  string `json:"echo"`
+	Value int    `json:"value"`
+}
+
+type AddMessage struct {
+	Type  string `json:"type"`
+	Delta int    `json:"delta"`
+}
+
+type AddMessageResponse struct {
+	Type string `json:"type"`
 }
 
 func NewServer(n *maelstrom.Node) *Server {
-	s := &Server{node: n}
+	kv := maelstrom.NewSeqKV(n)
+	s := &Server{node: n, kv: kv}
 
 	s.node.Handle("init", s.initHandler)
-	s.node.Handle("echo", s.echoHandler)
+	s.node.Handle("read", s.readHandler)
+	s.node.Handle("add", s.addHandler)
 
 	return s
 }
@@ -46,19 +61,66 @@ func (s *Server) initHandler(msg maelstrom.Message) error {
 	return nil
 }
 
-func (s *Server) echoHandler(msg maelstrom.Message) error {
-	var body EchoMessage
+func (s *Server) readHandler(msg maelstrom.Message) error {
+	var body ReadMessage
 	if err := json.Unmarshal(msg.Body, &body); err != nil {
 		return err
 	}
 
-	echoMessageResponse := EchoMessageResponse{
-		Type:  "echo_ok",
-		MsgID: body.MsgID,
-		Echo:  body.Echo,
+	ctx := context.Background()
+	fenceKey := fmt.Sprintf("fence-%d", rand.Int())
+	if err := s.kv.Write(ctx, fenceKey, 1); err != nil {
+		return err
 	}
 
-	return s.node.Reply(msg, echoMessageResponse)
+	counter, err := s.readCounter(ctx)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Read counter: %d", counter)
+
+	return s.node.Reply(msg, ReadMessageResponse{Type: "read_ok", Value: counter})
+}
+
+func (s *Server) addHandler(msg maelstrom.Message) error {
+	var body AddMessage
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+	log.Printf("Adding delta: %d", body.Delta)
+	for {
+		cur, err := s.readCounter(ctx)
+		if err != nil {
+			return err
+		}
+
+		err = s.kv.CompareAndSwap(ctx, counterKey, cur, cur+body.Delta, true)
+		if err == nil {
+			log.Printf("CAS succeeded: %d -> %d", cur, cur+body.Delta)
+			break
+		}
+		if rpcErr, ok := errors.AsType[*maelstrom.RPCError](err); ok && rpcErr.Code == maelstrom.PreconditionFailed {
+			log.Printf("CAS conflict at %d, retrying", cur)
+			continue
+		}
+		return err
+	}
+
+	return s.node.Reply(msg, AddMessageResponse{Type: "add_ok"})
+}
+
+func (s *Server) readCounter(ctx context.Context) (int, error) {
+	val, err := s.kv.ReadInt(ctx, counterKey)
+	if err != nil {
+		if rpcErr, ok := errors.AsType[*maelstrom.RPCError](err); ok && rpcErr.Code == maelstrom.KeyDoesNotExist {
+			return 0, nil
+		}
+		return 0, err
+	}
+	return val, nil
 }
 
 func (s *Server) Run() error {
